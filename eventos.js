c043abd119b9dd878e5c196fcc691550f85fe916
emprincessniall <emiliarodriguez834@gmail.com>
2023-08-09 14:56:57 -0300

    alert hola soy el div

---
+++ eventos.js
@@ -0,0 +1,9 @@
+var divElement = document.getElementById("div");
+
+
+        divElement.addEventListener("click", function(div) {
+            alert("Hola! Soy el div");
+        
+        
+
+        });
